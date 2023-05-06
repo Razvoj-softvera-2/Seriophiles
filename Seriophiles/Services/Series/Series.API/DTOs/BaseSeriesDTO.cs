bebe7eac3686fb3a1d7e817ca21776637fbbfe61
ivanacvetkoski <ivana.cvetkoski@gmail.com>
2023-05-06 20:35:36 +0200

    Feat: Added Entity and DTOs for Series

---
+++ Seriophiles/Services/Series/Series.API/DTOs/BaseSeriesDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Series.API.DTOs
+{
+    public class BaseSeriesDTO
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string[] Genres { get; set; }
+        public int Year { get; set; }
+        public string Runtime { get; set; }
+        public string[] Streaming { get; set; }
+        public string Description { get; set; }
+        public int Seasons { get; set; }
+        public int Episodes { get; set; }
+        public string[] Cast { get; set; }
+        public string Site { get; set; }
+    }
+}
---
+++ Seriophiles/Services/Series/Series.API/DTOs/Series.cs
@@ -0,0 +1,6 @@
+﻿namespace Series.API.DTOs
+{
+    public class Series : BaseSeriesDTO
+    {
+    }
+}
