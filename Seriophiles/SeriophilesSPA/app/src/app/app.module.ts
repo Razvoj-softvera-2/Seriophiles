aeb9613cf44d3dbecb06373b7492714b719ec1e1
Stevan Dragović <stevandragovic.10@gmail.com>
2023-08-05 20:45:35 +0200

    Added login and signup page

--- Seriophiles/SeriophilesSPA/app/src/app/app.module.ts
+++ Seriophiles/SeriophilesSPA/app/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { NavComponent } from './nav/nav.component';
 import { RouterOutlet } from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 import { MatListModule } from "@angular/material/list";
-import {MatToolbarModule} from "@angular/material/toolbar";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import { HttpClientModule } from "@angular/common/http";
+import {IdentityModule} from "./identity/identity.module";
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HttpClientModule } from "@angular/common/http";
     MatListModule,
     MatToolbarModule,
     MatIconModule,
-    HttpClientModule
+    HttpClientModule,
+    IdentityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Seriophiles/SeriophilesSPA/app/src/app/identity/identity.module.ts
+++ Seriophiles/SeriophilesSPA/app/src/app/identity/identity.module.ts
@@ -3,15 +3,37 @@ import { CommonModule } from '@angular/common';
 
 import { IdentityRoutingModule } from './identity-routing.module';
 import { IdentityComponent } from './identity.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {ReactiveFormsModule} from "@angular/forms";
+//import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
+//import {MatInput} from "@angular/material/input";
+//import {MatButton} from "@angular/material/button";
+//import {MatFormField} from "@angular/material/form-field";
 
 
 @NgModule({
   declarations: [
-    IdentityComponent
+    IdentityComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    UserInfoComponent
+  ],
+  exports: [
+    LoginFormComponent,
+    SignupFormComponent
   ],
   imports: [
     CommonModule,
-    IdentityRoutingModule
+    IdentityRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ]
 })
 export class IdentityModule { }
