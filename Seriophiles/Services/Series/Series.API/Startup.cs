c81022824409281f30945387733c9ff2c048bec3
ivanacvetkoski <ivana.cvetkoski@gmail.com>
2023-08-21 14:32:53 +0200

    Add authorization in startup

--- Seriophiles/Services/Series/Series.API/Startup.cs
+++ Seriophiles/Services/Series/Series.API/Startup.cs
@@ -3,6 +3,9 @@ using Microsoft.OpenApi.Models;
 using Series.API.Context;
 using Series.API.Entities;
 using Series.API.DTOs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Series.API
 {
@@ -37,12 +40,34 @@ namespace Series.API
                 configuration.CreateMap<Actor, ActorDTO>().ReverseMap();
             });
 
-            
-
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Series.API", Version = "v1" });
             });
+
+            var jwtSettings = Configuration.GetSection("JwtSettings");
+            var secretKey = jwtSettings.GetSection("secretKey").Value;
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = jwtSettings.GetSection("validIssuer").Value,
+                    ValidAudience = jwtSettings.GetSection("validAudience").Value,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +83,7 @@ namespace Series.API
             app.UseCors("CorsPolicy");
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
