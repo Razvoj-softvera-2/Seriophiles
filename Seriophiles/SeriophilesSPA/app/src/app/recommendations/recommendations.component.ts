960ebc933e908cb256f28a133edbd601637f0a8f
Stevan Dragović <stevandragovic.10@gmail.com>
2023-09-01 23:02:55 +0200

    Latest changes

--- Seriophiles/SeriophilesSPA/app/src/app/recommendations/recommendations.component.ts
+++ Seriophiles/SeriophilesSPA/app/src/app/recommendations/recommendations.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ITvShow} from "../shows/domain/models/ITvShow";
+import {TvShowFacadeService} from "../shows/domain/application-services/tv-show-facade.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-recommendations',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recommendations.component.css']
 })
 export class RecommendationsComponent {
+  public tvShows: ITvShow[] = [];
 
+  constructor(private tvShowFacadeService: TvShowFacadeService, private router: Router){
+    tvShowFacadeService.getTvShowsByYear(2023).subscribe((result:ITvShow[]) =>{
+      this.tvShows = [...result];
+      console.log(this.tvShows);
+    });
+  }
 }
