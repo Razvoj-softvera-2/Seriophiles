b94ec9e5d713b503d21101bb558b7d3784a6b8ba
Stevan Dragović <stevandragovic.10@gmail.com>
2023-09-01 23:19:24 +0200

    Latest changes

--- Seriophiles/SeriophilesSPA/app/src/app/recommendations/recommendations.component.ts
+++ Seriophiles/SeriophilesSPA/app/src/app/recommendations/recommendations.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import {ITvShow} from "../shows/domain/models/ITvShow";
 import {TvShowFacadeService} from "../shows/domain/application-services/tv-show-facade.service";
 import {Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {IAppState} from "../shared/app-state/app-state";
+import {AppStateService} from "../shared/app-state/app-state-service";
 
 @Component({
   selector: 'app-recommendations',
@@ -10,11 +13,13 @@ import {Router} from "@angular/router";
 })
 export class RecommendationsComponent {
   public tvShows: ITvShow[] = [];
+  public appState$: Observable<IAppState>;
 
-  constructor(private tvShowFacadeService: TvShowFacadeService, private router: Router){
+  constructor(private tvShowFacadeService: TvShowFacadeService, private router: Router, private appStateService: AppStateService){
     tvShowFacadeService.getTvShowsByYear(2023).subscribe((result:ITvShow[]) =>{
       this.tvShows = [...result];
       console.log(this.tvShows);
     });
+    this.appState$ = this.appStateService.getAppState();
   }
 }
